f73309e335e4c10fb73ff91ed1be09199c99c822
Kristina Stefanova <kris.stefanova@gmail.com>
2018-06-26 22:54:32 +1000

    Add another group of notes and play in a thread

--- melodies/amelie.rb
+++ melodies/amelie.rb
@@ -1,50 +1,81 @@
 @sleep = 0.3
 
-2.times do
+def row1
+  2.times do
+    use_synth :piano
+    play :E3
+    play :E4
+    sleep @sleep
+    play :B4
+    sleep @sleep
+    play :E4
+    play :G3
+    sleep @sleep
+    play :B4
+    sleep @sleep
+  end
+  
+  2.times do
+    use_synth :piano
+    play :D3
+    play :D4
+    sleep @sleep
+    play :B4
+    sleep @sleep
+    play :G3
+    play :D4
+    sleep @sleep
+    play :B4
+    sleep @sleep
+  end
+  
+  4.times do
+    use_synth :piano
+    play :D3
+    play :D4
+    sleep @sleep
+    play :B4
+    sleep @sleep
+    play :F3
+    play :D4
+    sleep @sleep
+    play :B4
+    sleep @sleep
+  end
+end
+
+def row2
   use_synth :piano
-  play :E2
-  play :E3
   sleep @sleep
-  play :B2
-  sleep @sleep
-  play :E3
-  play :G2
-  sleep @sleep
-  play :B2
-  sleep @sleep
+  
+  [:B4, :A4, :B4, :D4, :E4, :D4].each do |tone|
+    play tone
+    sleep @sleep
+  end
+  
+  sleep @sleep*3
+  [:A3, :B3, :A3, :B3, :C3, :B3].each do |tone|
+    play tone
+    sleep @sleep
+  end
+  
+  sleep @sleep*3
+  [:A3, :G2, :A3, :D3, :E3, :D3].each do |tone|
+    play tone
+    sleep @sleep
+  end
+  
+  sleep @sleep*3
+  [:A3, :G2, :A3].each do |tone|
+    play tone
+    sleep @sleep
+  end
 end
 
-2.times do
-  play :D2
-  play :D3
-  sleep @sleep
-  play :B3
-  sleep @sleep
-  play :G2
-  play :D3
-  sleep @sleep
-  play :B3
+row1()
+
+in_thread do
+  row2()
 end
 
-2.times do
-  play :D2
-  play :D3
-  sleep @sleep
-  play :B3
-  sleep @sleep
-  play :F2
-  play :G2
-  sleep @sleep
-  play :B3
-end
-
-2.times do
-  play :D2
-  play :D3
-  sleep @sleep
-  play :B3
-  sleep @sleep
-  play :F2
-  sleep @sleep
-  play :B3
-end
+row1
